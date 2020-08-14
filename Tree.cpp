b26d47218f8c1d6e9203ab4650d7b11d37454a0d
Joe X <xfreku@gmail.com>
2020-08-14 15:00:22 +0200

    Tree traversal

---
+++ Tree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int value;
+	Node* left, *right;
+};
+
+void inorder(Node* root) {
+	if (root) {
+		inorder(root->left);
+		cout<<root->value<<" -> ";
+		inorder(root->right);
+	}
+}
+
+void preorder(Node* root) {
+	if (root) {
+		cout<<root->value<<" -> ";
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void postorder(Node* root) {
+	if (root) {
+		postorder(root->left);
+		postorder(root->right);
+		cout<<root->value<<" -> ";
+	}
+}
+
+int main() {
+	Node* n1 = new Node {1, NULL, NULL};
+	Node* n2 = new Node {2, NULL, NULL};
+	Node* n3 = new Node {3, NULL, NULL};
+	Node* n4 = new Node {4, NULL, NULL};
+	Node* n5 = new Node {5, NULL, NULL};
+	Node* n6 = new Node {6, NULL, NULL};
+
+	n1->left = n2;
+	n1->right = n3;
+
+	n2->left = n4;
+	n2->right = n5;
+
+	n3->left = n6;
+	inorder(n1);
+	cout<<endl;
+	preorder(n1);
+	cout<<endl;
+	postorder(n1);
+	cout<<endl;
+	return 0;
+}
