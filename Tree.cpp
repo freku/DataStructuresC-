a7d4cd60547f21f829fd77bc30b945602c56b160
Joe X <xfreku@gmail.com>
2020-08-15 16:12:17 +0200

    tree search, insert

--- Tree.cpp
+++ Tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -43,37 +44,83 @@ bool hasNoChildren(Node* root) {
 	return !root->left && !root->right;
 }
 
+int depth(Node* root, int d) {
+	if (root) {
+		return max(depth(root->left, d + 1), depth(root->right, d + 1));
+	} else {
+		return d;
+	}
+}
+
 bool isFullBT(Node* root) {
 	if (!root || hasNoChildren(root))
 		return true;
 
-	if (root->left && root->right)
+	if (hasChildren(root))
 		return isFullBT(root->left) && isFullBT(root->right);
 
 	return false;
 }
 
+bool isPerfectBT(Node* root) {
+	if (hasNoChildren(root))
+		return true;	
+
+	if (root->left && root->right)
+		return depth(root->left, 0) == depth(root->right, 0)
+			&& isPerfectBT(root->left) && isPerfectBT(root->right);
+
+	return false;
+}
+
+Node* create_node(int value) {
+	return new Node {value, NULL, NULL};
+}
+
+void insert(Node*& root, int value) {
+	if (!root) {
+		root = create_node(value);
+		return;
+	}
+
+	if (value < root->value)
+		insert(root->left, value);
+	else
+		insert(root->right, value);
+}
+
+bool search(Node* root, int val) {
+	if (!root) return false;
+
+	if (root->value == val)
+		return true;
+
+	if (val < root->value)
+		return search(root->left, val);
+	else
+		return search(root->right, val);
+}
+
 int main() {
-	Node* n1 = new Node {1, NULL, NULL};
-	Node* n2 = new Node {2, NULL, NULL};
-	Node* n3 = new Node {3, NULL, NULL};
-	Node* n4 = new Node {4, NULL, NULL};
-	Node* n5 = new Node {5, NULL, NULL};
-	Node* n6 = new Node {6, NULL, NULL};
+	Node* root = create_node(12);
+	insert(root, 5);
+	insert(root, 6);
+	insert(root, 7);
+	insert(root, 14);
+	insert(root, 13);
+	insert(root, 16);
 
-	n1->left = n2;
-	n1->right = n3;
-
-	n2->left = n4;
-	n2->right = n5;
-
-	//n3->left = n6;
-	inorder(n1);
+	inorder(root);
 	cout<<endl;
-	preorder(n1);
+	preorder(root);
 	cout<<endl;
-	postorder(n1);
+	postorder(root);
 	cout<<endl;
-	cout<<isFullBT(n1)<<endl;
+	cout<<isFullBT(root)<<endl;
+	cout<<"is perfect: "<<isPerfectBT(root)<<endl;
+	cout<<"search 12: "<<search(root, 12)<<endl;
+	cout<<"search 5: "<<search(root, 5)<<endl;
+	cout<<"search 2: "<<search(root, 2)<<endl;
+	cout<<"search 1: "<<search(root, 1)<<endl;
 	return 0;
 }
